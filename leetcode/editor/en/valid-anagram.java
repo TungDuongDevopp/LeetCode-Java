fe366eb86085b02d699d7afb75f4c44b8cdc88a6
TungDuong-Dev <devopptungduong@gmail.com>
2026-06-06 21:57:18 +0700

    Solve Valid Anagram

---
+++ leetcode/editor/en/valid-anagram.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length()!= t.length()) return false;
+        Map <Character,Integer> map= new HashMap<>();
+
+        for(char c : s.toCharArray()) {
+            map.merge(c, 1, Integer::sum);
+
+        }
+
+        for(char c : t.toCharArray()) {
+            if(map.getOrDefault(c, 0) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+        return true;
+
+    }
+}
+
