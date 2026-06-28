97ff8759a5e052c21f5e7ab2eea5f16d5dcb2626
TungDuong-Dev <devopptungduong@gmail.com>
2026-06-28 19:22:43 +0700

    Solve Subarrays with K diff int

--- leetcode/editor/en/subarrays-with-k-different-integers.java
+++ leetcode/editor/en/subarrays-with-k-different-integers.java
@@ -1,9 +1,31 @@
-
 import java.util.*;
 
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+
+    private int atMostK(int[] nums, int k) {
+        if (k < 0) return 0;
+        //Khai báo biến đếm tần số
+        Map <Integer,Integer> map = new HashMap<>();
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            // Thêm phần tử bên phải vào cửa sổ
+            map.merge(nums[right],1,Integer::sum);
+
+            // Thu nhỏ cửa sổ từ bên trái nếu số lượng phần tử khác nhau vượt quá k
+            while (map.size() > k && left<= right ) {
+               map.merge(nums[left],-1,Integer::sum);
+               if(map.get(nums[left])==0) map.remove(nums[left]);
+               left++;
+            }
+            // Cộng dồn số lượng mảng con hợp lệ kết thúc tại 'right'
+            ans += right - left + 1;
+        }
+
+        return ans;
     }
 }
-
