43501e96b57be65e0515ca2b31143cbaebb2e5ba
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-02 18:08:27 +0700

    Solve Subarray Sum equals K

--- leetcode/editor/en/subarray-sum-equals-k.java
+++ leetcode/editor/en/subarray-sum-equals-k.java
@@ -3,7 +3,21 @@ import java.util.*;
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); // prefix = 0 xuất hiện 1 lần
+
+        int prefix = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            prefix += num;
+
+            ans += map.getOrDefault(prefix - k, 0);
+
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+        }
+        return ans;
     }
 }
 
