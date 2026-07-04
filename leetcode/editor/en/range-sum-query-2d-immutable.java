2179cef1b31310622005a4f9634f4c14030f91d0
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-05 00:44:55 +0700

    Solve Range sum query 2D

--- leetcode/editor/en/range-sum-query-2d-immutable.java
+++ leetcode/editor/en/range-sum-query-2d-immutable.java
@@ -1,20 +1,34 @@
 
 import java.util.*;
-//leetcode submit region begin(Prohibit modification and deletion)
+
 class NumMatrix {
 
+    private int[][] prefix;
+
     public NumMatrix(int[][] matrix) {
-        
+
+        int R = matrix.length;
+        int C = matrix[0].length;
+        prefix = new int[R + 1][C + 1];
+
+        // Dựng mảng prefix sum 2D
+        for (int i = 1; i <= R; i++) {
+            for (int j = 1; j <= C; j++) {
+                prefix[i][j] = prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1]
+                        + matrix[i - 1][j - 1]; // ma trận gốc dùng chỉ số dịch lại 1 đơn vị
+            }
+        }
     }
-    
+
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        
+        // Tính toán dựa trên công thức bù trừ diện tích hình chữ nhật
+        return prefix[row2 + 1][col2 + 1]
+                - prefix[row1][col2 + 1]
+                - prefix[row2 + 1][col1]
+                + prefix[row1][col1];
     }
 }
 
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
-//leetcode submit region end(Prohibit modification and deletion)
+
