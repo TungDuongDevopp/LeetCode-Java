5d87c6c513538d0eb4b8540d2c8f1e1b83d141cb
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-15 19:52:41 +0700

    Solve Minimum Speed to Arrive on time

---
+++ leetcode/editor/en/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int left = 1;
+        long maxDis = Long.MIN_VALUE;
+        int minSpeed = -1;
+
+        if (hour <= (double) dist.length - 1.0) {
+            return -1;
+        }
+
+        int right = 10000000;
+
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            if(checkSpeed(dist,hour,mid)){
+                 minSpeed = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return minSpeed;
+
+    }
+    private boolean checkSpeed(int[] dist, double hour, int speed){
+        double totalTime = 0.0;
+        for (int i = 0; i < dist.length - 1; i++) {
+            //Làm tròn thời gian trừ chuyến cuối
+            totalTime += (dist[i] + speed - 1)/speed;
+        }
+        // Chuyến cuối không phải tốn thời gian đợi do đã đến đích
+        totalTime += (double) dist[dist.length - 1] / speed;
+
+        return totalTime <= hour;
+    }
+}
